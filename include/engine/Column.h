e5d8db1a1f73fbf80cde24c7137ac0b662a9a107
Jhony Angulo <jhony.angulo@utec.edu.pe>
2019-05-26 23:13:02 -0500

    Add table

---
+++ include/engine/Column.h
@@ -0,0 +1,65 @@
+#ifndef COLUMN_H
+#define COLUMN_H
+#include <string>
+class Column {
+	typedef std::string string;
+	string name;
+	string data_type;
+	int data_length;
+	bool is_null;
+	bool is_pk;
+ public:
+	Column(std::string, std::string, int, bool, bool);
+	Column(const Column&);
+	~Column() {};
+	string getName() const;
+	string getDataType() const;
+	int getDataLength() const;
+	bool getIsNull() const;
+	bool getIsPK() const;
+	friend std::ostream& operator<<(std::ostream& os, const Column& d);
+};
+
+Column::Column(string name, string data_type, int data_length=10, bool is_null=false, bool is_pk=false)
+	: name(name),
+	  data_type(data_type),
+	  data_length(data_length),
+	  is_null(is_null),
+	  is_pk(is_pk)
+{}
+
+Column::Column(const Column &c)
+	: Column(c.name, c.data_type, c.data_length, c.is_null, c.is_pk)
+{}
+
+Column::string Column::getName() const{
+	return name;
+}
+
+Column::string Column::getDataType() const{
+	return data_type;
+}
+
+int Column::getDataLength() const{
+	return data_length;
+}
+
+bool Column::getIsNull() const
+{
+	return is_null;
+}
+
+bool Column::getIsPK() const{
+	return is_pk;
+}
+
+std::ostream &operator<<(std::ostream &os, const Column &col) {
+	os << col.getName() << " "
+	   << col.getDataType() << " "
+	   << std::to_string(col.getDataLength()) << " "
+	   << std::to_string(col.getIsNull()) << " "
+	   << std::to_string(col.getIsPK());
+	return os;
+}
+
+#endif
---
+++ include/engine/Row.h
@@ -0,0 +1,61 @@
+#ifndef ROW_H
+#define ROW_H
+
+#include <string>
+#include <map>
+#include <vector>
+#include "Column.h"
+
+class Row
+{
+	int index;
+	std::map<std::string, std::string> data;
+public:
+	Row(const std::vector<Column>&, std::vector<std::string>, int);
+	Row(const std::vector<std::pair<Column, std::string>> &, int);
+	void updateData(const Column&, const std::string&);
+	std::string getColumnValue(Column);
+	friend std::ostream& operator<<(std::ostream& os, const Row& c);
+};
+
+Row::Row(const std::vector<Column>& columns, std::vector<std::string> value, int index = 0)
+	: index(index)
+{
+	for (int i = 0; i < columns.size(); i++) {
+		data[columns[i].getName()] = value[i];
+	}
+}
+
+Row::Row(const std::vector<std::pair<Column, std::string>>& data, int index)
+	: index(index)
+{
+	for (auto &el : data) {
+		this->data[el.first.getName()] = el.second;
+	}
+}
+
+void Row::updateData(const Column& c, const std::string &value) {
+	if (value == "" && !c.getIsNull())
+		//error: cant be null
+		return; 
+	if (!data.count(c.getName()))
+		//error: no column
+		return;
+	data[c.getName()] = value;
+}
+
+std::string Row::getColumnValue(Column c) {
+	if( !row.count(c.getName()) )
+		//error: no column
+		return "";
+	return data[c.getName()];
+}
+
+std::ostream &operator<<(std::ostream &os, const Row &c) {
+	for (auto &el : c.data) {
+		os<<el.second<<" ";
+	}
+	return os;
+}
+
+#endif
